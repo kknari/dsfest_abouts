fbe611aad13aa614de554e35bde0de8cdfaa2beb
kknari <nari907@duksung.ac.kr>
2023-05-06 21:43:25 +0900

    서진이의 피드백

--- prj/abouts/urls.py
+++ prj/abouts/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
 
 from . import views
 
-router = routers.DefaultRouter()
-router.register('abouts', views.PostViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('abouts/', views.PostList.as_view()),
 ]
--- prj/abouts/views.py
+++ prj/abouts/views.py
@@ -1,11 +1,20 @@
-from rest_framework import viewsets
-
-from django.shortcuts import render
 from .models import Post
-from .serializers import PostCreateSerializer
-from .permissions import CustomReadOnly
+from .serializers import PostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostCreateSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from django.http import Http404
+
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True) # 여러 개 객체 serialization 위해 many=true
+        return Response(serializer.data)
     
+    def post(self, request):
+        serializer = PostSerializer(data=request.data) # 사용자의 입력 데이터
+        if serializer.is_valid(): # 유효성 검사
+            serializer.save() #저장
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
