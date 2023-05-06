849e2e8775800916c84eaaae514f613f148cefdf
kknari <nari907@duksung.ac.kr>
2023-05-06 23:43:08 +0900

    apiview로 변경

--- prj/abouts/urls.py
+++ prj/abouts/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 
 from . import views
 
-post_list = views.PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-
 urlpatterns = [
-    path('abouts/', post_list),
+    path('abouts/', views.PostList.as_view()),
 ]
